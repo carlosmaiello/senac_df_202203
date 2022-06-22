7887d648be4bfe901d76cb23d6840ad8e1aebff2
CARLOS ALBERTO MAIELLO JUNIOR <carlos.amjunior2@senacsp.edu.br>
2022-06-22 19:31:55 -0300

    Lista de produtos na Home

--- aula14/loja/src/pages/loja/components/ProductCategoryList.jsx
+++ aula14/loja/src/pages/loja/components/ProductCategoryList.jsx
@@ -4,23 +4,24 @@ import { Context } from "../context/ProductContext";
 import ProductCard from "./ProductCard";
 
 export default function CategoriaProdutos({ category }) {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState();
   const { getProductsByCategory } = useContext(Context);
 
   useEffect(() => {
     getProductsByCategory(category).then((response) => setProducts(response));
   }, [category]);
 
-  if (!products) return;
   return (
     <div>
       <h3>{category}</h3>
       <Row>
-        {products.map((product) => (
-          <Col md={4} className="my-3 d-flex align-items-stretch">
-            <ProductCard product={product} />
-          </Col>
-        ))}
+        {!products && <p>Carregando ...</p>}
+        {products &&
+          products.map((product) => (
+            <Col md={4} className="my-3 d-flex align-items-stretch">
+              <ProductCard product={product} />
+            </Col>
+          ))}
       </Row>
     </div>
   );
